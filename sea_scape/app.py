80812702b9d3573f569502049ffc7784499c59c6
Dylan Januszewski <Djanusze@bu.edu>
2023-09-07 17:50:03 -0400

    Added sound, scaling enemy spawn, and started creating a highscore board and adjusting hitbox of ship

--- sea_scape/app.py
+++ sea_scape/app.py
@@ -1,5 +1,6 @@
 import pygame
 from random import randint
+import sqlite3
 
 ocean= [6,5,4,3,2,1,0,-1,-2,-1,0,1,2,3,4]
 crate_loop = 0
@@ -7,6 +8,7 @@ k_list_loop = 0
 freq_loop = 0
 score = 0
 health = 3
+dif = 0
 game_active = False
 start_time = 0
 
@@ -19,6 +21,8 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Daydream_3/Daydream.ttf', 10)
 small_font = pygame.font.Font(None, 25)
 label_font = pygame.font.Font('font/Daydream_3/Daydream.ttf', 15)
+bg_music = pygame.mixer.Sound('audio/New Project.mp3')
+bg_music.play(loops = -1)
 
 #Game Over/ New Game Screen
 game_over_surf = pygame.image.load('graphics/bg/gameover.png').convert_alpha()
@@ -36,8 +40,16 @@ line8_surf = label_font.render(f"Space to start", True, 'Black' )
 #Ship image and rect
 ship_surf = pygame.image.load('graphics/ship/pship.png').convert_alpha()
 ship_rect = ship_surf.get_rect(midbottom = (400, 350))
+cannon_sound = kd_sound = bg_music = pygame.mixer.Sound('audio/cannon.wav')
+hitboat_sound = kd_sound = bg_music = pygame.mixer.Sound('audio/hitboat.wav')
+
 p_surf = pygame.image.load('graphics/enemy/cb.png').convert_alpha()
+p_surf = pygame.transform.smoothscale_by(p_surf, .5) 
 p_rect = p_surf.get_rect(center = (ship_rect.x, ship_rect.y) )
+
+rp_surf = pygame.transform.flip(p_surf, True, False)
+rp_rect = rp_surf.get_rect(center = (ship_rect.x, ship_rect.y) )
+
 
 #crate image and rect
 crate_surf = pygame.image.load('graphics/help/crate.png').convert_alpha()
@@ -60,13 +72,17 @@ k_rect = k_surf.get_rect(midbottom = (700, 375))
 rk_surf = pygame.transform.flip(k_surf, True, False)
 rk_rect = rk_surf.get_rect(midbottom = (100, 375))
 
+kd_sound = bg_music = pygame.mixer.Sound('audio/kd.wav')
+
 #enemy logic
 enemy_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(enemy_timer, 1500)
 ct = int((pygame.time.get_ticks() - start_time)/ 1000)
-pygame.time.set_timer(enemy_timer, int(10000/(ct+1)))
+
 
 cb_rect_list = []
 k_rect_list = []
+p_rect_list = []
 
 def cb_movement(cb_list):
     global health
@@ -74,10 +90,10 @@ def cb_movement(cb_list):
     if cb_list:
         for cb_rect in cb_list:
             cb_rect.y += 4
-
             screen.blit(cb_surf, cb_rect)
             if ship_rect.colliderect(cb_rect):
                 health -= 1
+                hitboat_sound.play(loops = 0)
         cb_list = [cb for cb in cb_list if ship_rect.colliderect(cb) == False]
         cb_list = [cb for cb in cb_list if cb.y < 500]
         return cb_list
@@ -86,23 +102,52 @@ def cb_movement(cb_list):
         return []
 
 def k_movement(k_list):
+    global health
+    global score
+    global p_rect_list
     if k_list:
         for k_rect in k_list:
             if k_rect.x > ship_rect.x:
                 k_rect.x -= 4
                 screen.blit(k_surf, k_rect)
+                if ship_rect.colliderect(k_rect):
+                    health -= 1
+                    hitboat_sound.play(loops = 0)
+            for p in p_rect_list:
+                if k_rect.colliderect(p):
+                    k_list.remove(k_rect)
+                    p_rect_list.remove(p)
+                    kd_sound.play(loops = 0)
+                    score += 1
             k_list = [k for k in k_list if ship_rect.colliderect(k) == False]
         
         for rk_rect in k_list:
             if rk_rect.x < ship_rect.x:
                 rk_rect.x += 4
                 screen.blit(rk_surf, rk_rect)
+                if ship_rect.colliderect(rk_rect):
+                    health -= 1
+                    hitboat_sound.play(loops = 0)
             k_list = [rk for rk in k_list if ship_rect.colliderect(rk) == False]
-
         return k_list
     else:
         return []
 
+def p_movement(p_list):
+    if p_list:
+        for p_rect in p_list:
+            if p_rect.centerx > ship_rect.centerx:
+                p_rect.x += 4
+                screen.blit(p_surf, p_rect) 
+            p_list = [p for p in p_list if p.x < 1000]  
+        for rp_rect in p_list: 
+            if rp_rect.centerx < ship_rect.centerx:        
+                rp_rect.x -= 4
+                screen.blit(rp_surf, rp_rect) 
+            p_list = [rp for rp in p_list if rp.x > -100]
+        return p_list
+    else:
+        return []
 # Animation for moving kraken
 def k_sway():
     global k_surf, k_index, rk_surf
@@ -123,7 +168,7 @@ def scored():
     global score
     score_text_surf = test_font.render(f'Score: {score}', False, 'Black' )
     if ship_rect.colliderect(crate_rect): 
-            score += 1
+            score += 5
             crate_rect.y = 500
     screen.blit(score_text_surf, (40,25))
 
@@ -156,18 +201,26 @@ while running:
         if game_active:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    print('fire')
+                    if len(p_rect_list) <= 2:
+                        cannon_sound.play(loops = 0)
+                        p_rect_list.append(p_surf.get_rect(center = ((ship_rect.centerx+5), ship_rect.centery)))
+                        p_rect_list.append(rp_surf.get_rect(center = ((ship_rect.centerx-5), ship_rect.centery)))
             if event.type == enemy_timer:
-                cb_rect_list.append(cb_surf.get_rect(midbottom = (randint(25, 775), randint(-200, -30))))
-                if k_list_loop == 10:
-                    k_list_loop = 0
-                    if randint(0,2):
-                        k_rect_list.append(rk_surf.get_rect(midbottom = (-300, 375)))
-                    else:
-                        k_rect_list.append(k_surf.get_rect(midbottom = (1000, 375)))
-                else:
-                    k_list_loop += 1
-                print(k_list_loop)
+                    print(ct)
+                    if ct % 15 == 0:
+                        print(ct)
+                        print(dif)
+                        dif += 1
+                    for i in range(dif):
+                        cb_rect_list.append(cb_surf.get_rect(midbottom = (randint(25, 775), randint(-200, -30))))
+                        if k_list_loop == 20:
+                            k_list_loop = 0
+                            if randint(0,2):
+                                k_rect_list.append(rk_surf.get_rect(midbottom = (-300, 375)))
+                            else:
+                                k_rect_list.append(k_surf.get_rect(midbottom = (1000, 375)))
+                        else:
+                            k_list_loop += 1
         else:
             #game start
             if event.type == pygame.KEYDOWN:
@@ -175,7 +228,12 @@ while running:
                     game_active = True
                     health = 3
                     score = 0
+                    dif = 0
+                    k_rect_list = []
+                    cb_rect_list = []
+                    crate_rect.y = 500
                     start_time = pygame.time.get_ticks()
+    ct = int((pygame.time.get_ticks() - start_time)/ 1000)
     user_input = pygame.key.get_pressed()
     if user_input[pygame.K_LEFT] and ship_rect.x >= -25:
         ship_rect.x -= 3
@@ -203,9 +261,9 @@ while running:
              game_active = False
              cb_rect.y = -50
              crate_rect.y =-50
-        screen.blit(ship_surf, ship_rect)
         cb_rect_list = cb_movement(cb_rect_list)
         k_rect_list = k_movement(k_rect_list)
+        p_rect_list = p_movement(p_rect_list)
         screen.blit(ship_surf, ship_rect)
 
         #crate reinitialize and movement logic 
